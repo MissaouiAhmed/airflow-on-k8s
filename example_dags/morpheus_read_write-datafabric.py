2c9c527aed4d5ca11d47e49346ffedcc2a360c73
MISSAOUI <ahmed.missaoui@hpe.com>
2025-04-22 16:15:09 +0200

    update morpheus dag

--- example_dags/morpheus_read_write-datafabric.py
+++ example_dags/morpheus_read_write-datafabric.py
@@ -14,8 +14,20 @@ with DAG(
     catchup=False,
 ) as dag:
 
+        # 1. Define the Volume (e.g., PVC)
+    volume = k8s.V1Volume(
+        name='my-shared-volume',
+        persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='test-datafabric-airflow-shared-volume')
+    )
 
-    create_file = KubernetesPodOperator(
+    # 2. Define the VolumeMount
+    volume_mount = k8s.V1VolumeMount(
+        mount_path='/mnt/datafabric-volume',  # Path inside the container
+        name='my-shared-volume',
+        read_only=False
+    )
+
+    list_files = KubernetesPodOperator(
         task_id='create_file',
         name='create-file',
         namespace='airflow',
@@ -23,8 +35,37 @@ with DAG(
         cmds=['bash', '-c'],
         labels={"app": "airflow"},
         arguments=["ls -ali /mnt"],
+        volumes=[volume],
+        volume_mounts=[volume_mount],
         is_delete_operator_pod=True,
     )
 
-    create_file 
+create_file = KubernetesPodOperator(
+        task_id='create_file',
+        name='create-file',
+        namespace='airflow',
+        image='bash:latest',
+        cmds=['bash', '-c'],
+        labels={"app": "airflow"},
+        arguments=["echo 'Hello from Airflow morpheus volume!' > /mnt/datafabric-volume/morpheus-airflow.txt"],
+        volumes=[volume],
+        volume_mounts=[volume_mount],
+        is_delete_operator_pod=True,
+    )
+
+    read_file = KubernetesPodOperator(
+        task_id='read_file',
+        name='read-file',
+        namespace='airflow',
+        image='bash:latest',
+        cmds=['bash', '-c'],
+        labels={"app": "airflow"},
+        arguments=["cat /mnt/datafabric-volume/morpheus-airflow.txt"],
+        volumes=[volume],
+        volume_mounts=[volume_mount],
+        is_delete_operator_pod=True,
+    )
+
+    list_files >> create_file >> read_file
+    
 
